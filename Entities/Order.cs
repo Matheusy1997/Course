c46ae4badc743d73889f98b71fcb32ac1717cff1
Matheusy1997 <matheusmachado.dev@gmail.com>
2024-09-19 18:41:44 -0300

    first commit

---
+++ Entities/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course2.Entities.Enums;
+using System.Globalization;
+
+namespace Course2.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItem { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItem.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItem.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem item in OrderItem)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Ordermoment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Orderstatus: " + Status);
+            sb.AppendLine("Client: " + Client);
+
+            foreach (OrderItem item in OrderItem)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: " + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Course2.Entities;
+using Course2.Entities.Enums;
+using System.Globalization;
+
+namespace Course2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birthdate (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter orderdata: ");
+
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("Howmanyitemstothis order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, price);
+                OrderItem item = new OrderItem(quantity,price, product);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine("ORDER SUMMARY: ");
+            Console.WriteLine(order);
+        }
+    }
+}
